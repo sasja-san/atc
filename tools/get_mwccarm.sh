07c1c68d0cefa7e6bc50eabcbaefea5c9fe3d7c6
Alexander Göransson <alexander.goransson@gmail.com>
2025-12-11 08:54:04 +0100

    Add alternate URL for compiler kit.

--- tools/get_mwccarm.sh
+++ tools/get_mwccarm.sh
@@ -2,9 +2,12 @@
 
 FNAME="mwccarm.zip"
 URL="http://decomp.aetias.com/files/mwccarm.zip"
+ALTERNATE_URL="https://twlsdk.randommeaninglesscharacters.com/download/mwccarm.zip"
 EXTRACTION_DIR="tools"
 
-printf "\nFetching %s. Zip file is 40 MB, uncompressed it's around 85 MB.\n" $FNAME
+printf "\nFetching %s.\n" $FNAME
+printf "\tZip file is 40 MB.\n"
+printf "\tUncompressed it's around 85 MB.\n\n" 
 wget $URL
 unzip $FNAME -d $EXTRACTION_DIR
 printf "\nDeleting file %s.\n" $FNAME
