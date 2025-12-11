0031c1d0f68ac3d80446af3a68f45f3898ecedcb
Alexander Göransson <alexander.goransson@gmail.com>
2025-12-11 07:40:37 +0100

    Compiler DL should be from project root

--- tools/get_mwccarm.sh
+++ tools/get_mwccarm.sh
@@ -2,10 +2,11 @@
 
 FNAME="mwccarm.zip"
 URL="http://decomp.aetias.com/files/mwccarm.zip"
+EXTRACTION_DIR="tools"
 
 printf "\nFetching %s. Zip file is 40 MB, uncompressed it's around 85 MB.\n" $FNAME
 wget $URL
-unzip $FNAME
+unzip $FNAME -d $EXTRACTION_DIR
 printf "\nDeleting file %s.\n" $FNAME
 rm $FNAME
 
